5f646be7351766ea01c4fb32b1d1f96e655b1037
David Rotermund <54365609+davrot@users.noreply.github.com>
2024-01-09 15:21:52 +0100

    Add files via upload

---
+++ squash_L1/CapsLayer_mod.py
@@ -0,0 +1,45 @@
+import torch
+
+
+class CapsLayer(torch.nn.Module):
+    input_dim: int
+    input_caps: int
+    output_dim: int
+    output_caps: int
+    weights: torch.nn.Parameter
+
+    def __init__(
+        self, input_caps: int, input_dim: int, output_caps: int, output_dim: int
+    ) -> None:
+        super(CapsLayer, self).__init__()
+        self.input_dim: int = input_dim
+        self.input_caps: int = input_caps
+        self.output_dim: int = output_dim
+        self.output_caps: int = output_caps
+
+        self.weights: torch.nn.Parameter = torch.nn.Parameter(
+            torch.Tensor(input_caps, input_dim, output_caps * output_dim)
+        )
+
+        self.bias: torch.nn.Parameter = torch.nn.Parameter(
+            torch.Tensor(input_caps, output_caps * output_dim)
+        )
+
+        self.reset_parameters()
+
+    def reset_parameters(self) -> None:
+        stdv: torch.Tensor = 1.0 / torch.sqrt(torch.tensor(self.input_caps))
+        self.weights.data.uniform_(-float(stdv), float(stdv))
+        k = 1.0 / float(self.input_dim)
+        self.bias.data.uniform_(-float(k), float(k))
+
+    def forward(self, input: torch.Tensor) -> torch.Tensor:
+        output = (self.weights.unsqueeze(0) * input.unsqueeze(-1)).sum(dim=-2)
+        output = output + self.bias.unsqueeze(0)
+
+        return output.reshape(
+            input.shape[0],
+            input.shape[1],
+            self.output_caps,
+            self.output_dim,
+        )
