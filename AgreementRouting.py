c7b2e1bb0caf77187cb0fc3038869e703da6a7af
mahbodnr <mahbodnouri@gmail.com>
2024-01-11 11:04:18 +0100

    change initial b values in routing similar to the paper

--- AgreementRouting.py
+++ AgreementRouting.py
@@ -9,18 +9,19 @@ class AgreementRouting(torch.nn.Module):
         super().__init__()
         assert n_iterations >= 0
         self.n_iterations: int = n_iterations
-        self.b: torch.nn.Parameter = torch.nn.Parameter(
-            torch.zeros((input_caps, output_caps))
-        )
+        self.input_caps: int = input_caps
+        self.output_caps: int = output_caps
+
 
     def forward(self, input: torch.Tensor) -> torch.Tensor:
-        s = (torch.nn.functional.softmax(self.b, dim=-1).unsqueeze(2) * input).sum(
+        b = torch.zeros((self.input_caps, self.output_caps)).to(input.device)
+        s = (torch.nn.functional.softmax(b, dim=-1).unsqueeze(2) * input).sum(
             dim=1
         )
         lengths2 = (s**2).sum(dim=2)
         v = s * (lengths2.sqrt() / (1 + lengths2)).unsqueeze(-1)
 
-        b_batch = self.b.unsqueeze(0)
+        b_batch = b.unsqueeze(0)
         for _ in range(0, self.n_iterations):
             b_batch = b_batch + (input * v.unsqueeze(1)).sum(dim=-1)
 
