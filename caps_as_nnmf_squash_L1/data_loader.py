eac0216293211c9c45643089d349251e67a13354
mahbodnr <mahbodnouri@gmail.com>
2024-01-11 16:26:46 +0100

    add fashion-mnist and cifar10 datasets

--- caps_as_nnmf_squash_L1/data_loader.py
+++ caps_as_nnmf_squash_L1/data_loader.py
@@ -3,33 +3,96 @@ import torchvision as tv
 
 
 def data_loader(
-    batch_size: int, test_batch_size: int
+    dataset: str, batch_size: int, test_batch_size: int
 ) -> tuple[torch.utils.data.DataLoader, torch.utils.data.DataLoader]:
-    train_loader = torch.utils.data.DataLoader(
-        tv.datasets.MNIST(
-            "../data",
-            train=True,
-            download=True,
-            transform=tv.transforms.Compose(
-                [
-                    tv.transforms.Pad(2),
-                    tv.transforms.RandomCrop(28),
-                    tv.transforms.ToTensor(),
-                ]
+    if dataset == "mnist":
+        train_loader = torch.utils.data.DataLoader(
+            tv.datasets.MNIST(
+                "~/data",
+                train=True,
+                download=True,
+                transform=tv.transforms.Compose(
+                    [
+                        tv.transforms.Pad(2),
+                        tv.transforms.RandomCrop(28),
+                        tv.transforms.ToTensor(),
+                    ]
+                ),
             ),
-        ),
-        batch_size=batch_size,
-        shuffle=True,
-    )
+            batch_size=batch_size,
+            shuffle=True,
+        )
 
-    test_loader = torch.utils.data.DataLoader(
-        tv.datasets.MNIST(
-            "../data",
-            train=False,
-            transform=tv.transforms.Compose([tv.transforms.ToTensor()]),
-        ),
-        batch_size=test_batch_size,
-        shuffle=False,
-    )
+        test_loader = torch.utils.data.DataLoader(
+            tv.datasets.MNIST(
+                "~/data",
+                train=False,
+                transform=tv.transforms.Compose([tv.transforms.ToTensor()]),
+            ),
+            batch_size=test_batch_size,
+            shuffle=False,
+        )
+    elif dataset == "fashion-mnist":
+        train_loader = torch.utils.data.DataLoader(
+            tv.datasets.FashionMNIST(
+                "~/data",
+                train=True,
+                download=True,
+                transform=tv.transforms.Compose(
+                    [
+                        tv.transforms.Pad(2),
+                        tv.transforms.RandomCrop(28),
+                        tv.transforms.ToTensor(),
+                    ]
+                ),
+            ),
+            batch_size=batch_size,
+            shuffle=True,
+        )
+
+        test_loader = torch.utils.data.DataLoader(
+            tv.datasets.FashionMNIST(
+                "~/data",
+                train=False,
+                transform=tv.transforms.Compose([tv.transforms.ToTensor()]),
+            ),
+            batch_size=test_batch_size,
+            shuffle=False,
+        )
+    elif dataset == "cifar10":
+        train_loader = torch.utils.data.DataLoader(
+            tv.datasets.CIFAR10(
+                "~/data",
+                train=True,
+                download=True,
+                transform=tv.transforms.Compose(
+                    [
+                        transforms.RandomCrop(28),
+                        transforms.RandomHorizontalFlip(0.5),
+                        transforms.ColorJitter(
+                            brightness=0.5, contrast=0.1, saturation=0.1, hue=0.15
+                        ),
+                        transforms.ToTensor(),
+                    ]
+                ),
+            ),
+            batch_size=batch_size,
+            shuffle=True,
+        )
+
+        test_loader = torch.utils.data.DataLoader(
+            tv.datasets.CIFAR10(
+                "~/data",
+                train=False,
+                transform=tv.transforms.Compose(
+                    [
+                        transforms.CenterCrop(28),
+                        transforms.ToTensor(),
+                    ]
+                ),
+            ),
+            batch_size=test_batch_size,
+            shuffle=False,
+        )
 
     return train_loader, test_loader
--- caps_as_nnmf_squash_L1/run_nnmf.py
+++ caps_as_nnmf_squash_L1/run_nnmf.py
@@ -29,45 +29,174 @@ torch_device: torch.device = (
 # -------
 parser = argparse.ArgumentParser(description="CapsNet NNMF")
 
-parser.add_argument("--batch-size", type=int, default=128, help="batch size (default: 128)")
-parser.add_argument("--test-batch-size", type=int, default=100, help="test batch size (default: 100)")
-parser.add_argument("--epochs", type=int, default=100, help="number of epochs to train (default: 100)")
+parser.add_argument(
+    "--epochs", type=int, default=100, help="number of epochs to train (default: 100)"
+)
 parser.add_argument("--seed", type=int, default=1, help="random seed (default: 1)")
-parser.add_argument("--log-interval", type=int, default=10, help="how many batches to wait before logging training status")
+parser.add_argument(
+    "--log-interval",
+    type=int,
+    default=10,
+    help="how many batches to wait before logging training status",
+)
+# -------
+# Dataset:
+parser.add_argument(
+    "--dataset",
+    type=str,
+    default="fashion-mnist",
+    help="dataset (default: mnist)",
+    choices=["mnist", "fashion-mnist", "cifar10"],
+)
+parser.add_argument(
+    "--batch-size", type=int, default=128, help="batch size (default: 128)"
+)
+parser.add_argument(
+    "--test-batch-size", type=int, default=100, help="test batch size (default: 100)"
+)
 # -------
 # Network structure:
-parser.add_argument("--number-of-classes", type=int, default=10, help="number of classes (default: 10)")
-parser.add_argument("--conv1-in-channels", type=int, default=1, help="conv1 in channels (default: 1)")
-parser.add_argument("--conv1-out-channels", type=int, default=256, help="conv1 out channels (default: 256)")
-parser.add_argument("--conv1-kernel-size", type=int, default=9, help="conv1 kernel size (default: 9)")
-parser.add_argument("--conv1-stride", type=int, default=1, help="conv1 stride (default: 1)")
-parser.add_argument("--conv2-kernel-size", type=int, default=9, help="conv2 kernel size (default: 9)")
-parser.add_argument("--conv2-stride", type=int, default=2, help="conv2 stride (default: 2)")
-parser.add_argument("--primary-caps-output-dim", type=int, default=8, help="primary caps output dim (default: 8)")
-parser.add_argument("--primary-caps-output-caps", type=int, default=32, help="primary caps output caps (default: 32)")
-parser.add_argument("--number-of-primary-caps-yx", type=int, default=36, help="number of primary caps yx (default: 36)")
-parser.add_argument("--caps-layer-output-dim", type=int, default=16, help="caps layer output dim (default: 16)")
-parser.add_argument("--fc1-out-features", type=int, default=512, help="fc1 out features (default: 512)")
-parser.add_argument("--fc2-out-features", type=int, default=1024, help="fc2 out features (default: 1024)")
-parser.add_argument("--fc3-out-features", type=int, default=784, help="fc3 out features (default: 784)")
+parser.add_argument(
+    "--number-of-classes", type=int, default=10, help="number of classes (default: 10)"
+)
+parser.add_argument(
+    "--conv1-in-channels", type=int, default=1, help="conv1 in channels (default: 1)"
+)
+parser.add_argument(
+    "--conv1-out-channels",
+    type=int,
+    default=256,
+    help="conv1 out channels (default: 256)",
+)
+parser.add_argument(
+    "--conv1-kernel-size", type=int, default=9, help="conv1 kernel size (default: 9)"
+)
+parser.add_argument(
+    "--conv1-stride", type=int, default=1, help="conv1 stride (default: 1)"
+)
+parser.add_argument(
+    "--conv2-kernel-size", type=int, default=9, help="conv2 kernel size (default: 9)"
+)
+parser.add_argument(
+    "--conv2-stride", type=int, default=2, help="conv2 stride (default: 2)"
+)
+parser.add_argument(
+    "--primary-caps-output-dim",
+    type=int,
+    default=8,
+    help="primary caps output dim (default: 8)",
+)
+parser.add_argument(
+    "--primary-caps-output-caps",
+    type=int,
+    default=32,
+    help="primary caps output caps (default: 32)",
+)
+parser.add_argument(
+    "--number-of-primary-caps-yx",
+    type=int,
+    default=36,
+    help="number of primary caps yx (default: 36)",
+)
+parser.add_argument(
+    "--caps-layer-output-dim",
+    type=int,
+    default=16,
+    help="caps layer output dim (default: 16)",
+)
+parser.add_argument(
+    "--fc1-out-features", type=int, default=512, help="fc1 out features (default: 512)"
+)
+parser.add_argument(
+    "--fc2-out-features",
+    type=int,
+    default=1024,
+    help="fc2 out features (default: 1024)",
+)
+parser.add_argument(
+    "--fc3-out-features", type=int, default=784, help="fc3 out features (default: 784)"
+)
 # reconstruction
-parser.add_argument("--with-reconstruction", action="store_true", default=False, help="use reconstruction loss (default: False)")
-parser.add_argument("--mask-reconstruction", action="store_true", default=True, help="mask reconstruction loss (default: True)")
-parser.add_argument("--reconstruction-alpha", type=float, default=0.0005, help="reconstruction loss weight (default: 0.0005)")
+parser.add_argument(
+    "--with-reconstruction",
+    action="store_true",
+    default=False,
+    help="use reconstruction loss (default: False)",
+)
+parser.add_argument(
+    "--mask-reconstruction",
+    action="store_true",
+    default=True,
+    help="mask reconstruction loss (default: True)",
+)
+parser.add_argument(
+    "--reconstruction-alpha",
+    type=float,
+    default=0.0005,
+    help="reconstruction loss weight (default: 0.0005)",
+)
 # loss function
-parser.add_argument("--m-pos", type=float, default=0.9, help="m+ parameter for margin loss (default: 0.9)")
-parser.add_argument("--m-neg", type=float, default=0.1, help="m- parameter for margin loss (default: 0.1)")
-parser.add_argument("--loss-lambda", type=float, default=0.5, help="lambda parameter for margin loss (default: 0.5)")
+parser.add_argument(
+    "--m-pos",
+    type=float,
+    default=0.9,
+    help="m+ parameter for margin loss (default: 0.9)",
+)
+parser.add_argument(
+    "--m-neg",
+    type=float,
+    default=0.1,
+    help="m- parameter for margin loss (default: 0.1)",
+)
+parser.add_argument(
+    "--loss-lambda",
+    type=float,
+    default=0.5,
+    help="lambda parameter for margin loss (default: 0.5)",
+)
 # optimizer
-parser.add_argument("--optimizer", type=str, default="adam", help="optimizer (default: adam)", choices=["adam", "madam"])
-parser.add_argument("--lr", type=float, default=0.001, help="learning rate (default: 0.001)")
-parser.add_argument("--patience", type=int, default=15, help="patience for ReduceLROnPlateau (default: 15)")
-parser.add_argument("--min-lr", type=float, default=1e-6, help="min lr for ReduceLROnPlateau (default: 1e-6)")
+parser.add_argument(
+    "--optimizer",
+    type=str,
+    default="adam",
+    help="optimizer (default: adam)",
+    choices=["adam", "madam"],
+)
+parser.add_argument(
+    "--lr", type=float, default=0.001, help="learning rate (default: 0.001)"
+)
+parser.add_argument(
+    "--patience",
+    type=int,
+    default=15,
+    help="patience for ReduceLROnPlateau (default: 15)",
+)
+parser.add_argument(
+    "--min-lr",
+    type=float,
+    default=1e-6,
+    help="min lr for ReduceLROnPlateau (default: 1e-6)",
+)
 # nnmf
-parser.add_argument("--nnmf-number-of-iterations", type=int, default=5, help="number of iterations for NNMF (default: 5)")
-parser.add_argument("--nnmf-threshold", type=float, default=0.00001, help="threshold for NNMF (default: 0.00001)")
+parser.add_argument(
+    "--nnmf-number-of-iterations",
+    type=int,
+    default=5,
+    help="number of iterations for NNMF (default: 5)",
+)
+parser.add_argument(
+    "--nnmf-threshold",
+    type=float,
+    default=0.00001,
+    help="threshold for NNMF (default: 0.00001)",
+)
 # model path
-parser.add_argument("--model-path", type=str, default="models", help="path to save models (default: models)")
+parser.add_argument(
+    "--model-path",
+    type=str,
+    default="models",
+    help="path to save models (default: models)",
+)
 # -------
 args = parser.parse_args()
 
@@ -76,7 +205,9 @@ if use_gpu:
     torch.cuda.manual_seed(args.seed)
 
 train_loader, test_loader = data_loader(
-    batch_size=args.batch_size, test_batch_size=args.test_batch_size
+    dataset=args.dataset,
+    batch_size=args.batch_size,
+    test_batch_size=args.test_batch_size,
 )
 model, probability_layer_position, mask_layer_position, nnmf_layer_position = network(
     number_of_classes=args.number_of_classes,
@@ -104,22 +235,35 @@ if args.optimizer == "adam":
     optimizer = torch.optim.Adam(model.parameters(), lr=args.lr)
     print("Using Adam optimizer.")
 elif args.optimizer == "madam":
-    nnmf_params = [p for n, p in model.named_parameters() if n.endswith("weights") or isinstance(p, NonNegativeParameter)]
-    other_params = [p for n, p in model.named_parameters() if not n.endswith("weights") and not isinstance(p, NonNegativeParameter)]
-    optimizer = Madam(params=[
-        {
-            "params": nnmf_params,
-            "lr": args.lr,
-            "NNMF": True,
-            "foreach": False,
-        },
-        {
-            "params": other_params,
-            "lr": args.lr,
-        },
-    ])
-    print(f"Using Madam optimizer on {len(nnmf_params)} nnmf parameters and {len(other_params)} other parameters.")
-
+    nnmf_params = [
+        p
+        for n, p in model.named_parameters()
+        if n.endswith("weights") or isinstance(p, NonNegativeParameter)
+    ]
+    other_params = [
+        p
+        for n, p in model.named_parameters()
+        if not n.endswith("weights") and not isinstance(p, NonNegativeParameter)
+    ]
+    optimizer = Madam(
+        params=[
+            {
+                "params": nnmf_params,
+                "lr": args.lr,
+                "NNMF": True,
+                "foreach": False,
+            },
+            {
+                "params": other_params,
+                "lr": args.lr,
+            },
+        ]
+    )
+    print(
+        f"Using Madam optimizer on {len(nnmf_params)} nnmf parameters and {len(other_params)} other parameters."
+    )
+else:
+    raise ValueError(f"Unknown optimizer: {args.optimizer}")
 
 scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(
     optimizer, verbose=True, patience=args.patience, min_lr=args.min_lr
@@ -172,7 +316,7 @@ for epoch in range(1, args.epochs + 1):
         model.state_dict(),
         os.path.join(
             model_dir,
-            f"{epoch:03d}_nnmf_model_dict_{args.nnmf_number_of_iterations}_iter_{'reconstruction' if args.with_reconstruction else ''}.pth",
+            f"{args.dataset}_{epoch:03d}_nnmf_model_dict_{args.nnmf_number_of_iterations}_iter{'_reconstruction' if args.with_reconstruction else ''}.pth",
         ),
     )
 
